ee5034dc05b196834ec51591defe948786a998d1
Dave Collier <davetheflashguy@gmail.com>
2014-06-03 13:18:38 -0400

    FIxes deep linking/scrolling

--- public/app.router.js
+++ public/app.router.js
@@ -4,59 +4,80 @@ define(function (require, exports, module) {
 	var mediator = require('framework/mediator');
 	var appLayoutTemplate = require('html!ui/_shared/layouts/app');
 
+	require(['ui/home/homeView'], function(HomeView){
+
+		new HomeView({layoutTemplate: appLayoutTemplate})
+
+	});
+	require(['ui/resume/resumeView'], function(HomeView){
+
+		new HomeView({layoutTemplate: appLayoutTemplate})
+
+	});
+	require(['ui/contact/contactView'], function(ContactView){
+
+		new ContactView({layoutTemplate: appLayoutTemplate})
+
+	});
 
 	require(['ui/menu/menuView'], function(MenuView){
 		var menu = new MenuView();
 				menu.render();
+
 
 		// menu listeners
 		  var scrolling_speed = 500;
 		  // handle navigation clicks
 		  $("#home-btn").click(function() {
 		    $('html, body').animate({
-		            scrollTop: $(".home-container").offset().top
+		            scrollTop: $(".home-container").offset().top - 50
 		        }, scrolling_speed);
 		  })
 		  $("#resume-btn").click(function() {
 		    $('html, body').animate({
-		            scrollTop: $(".resume-container").offset().top
+		            scrollTop: $(".resume-container").offset().top - 50
 		        }, scrolling_speed);
 		  })
 		  $("#contact-btn").click(function() {
 		    $('html, body').animate({
-		            scrollTop: $(".contact-container").offset().top
+		            scrollTop: $(".contact-container").offset().top - 50
 		        }, scrolling_speed);
 		  })
 			$("#hire-me-btn").click(function() {
 				$('html, body').animate({
-								scrollTop: $(".contact-container").offset().top
+								scrollTop: $(".contact-container").offset().top - 50
 						}, scrolling_speed);
 			})
 	});
 
 	return Backbone.Router.extend({
 
+
 		routes: {
-			'' : 'main'
+			'home' : 'home',
+			'resume' : 'resume',
+			'contact' : 'contact',
+			'hire-me' : 'contact',
+
 		},
 
-		main: function() {
-			require(['ui/home/homeView'], function(HomeView){
+		home: function() {
+			$('html, body').animate({
+							scrollTop: $(".home-container").offset().top - 50
+					}, 500);
+		},
 
-				new HomeView({layoutTemplate: appLayoutTemplate})
+		resume: function() {
+			$('html, body').animate({
+							scrollTop: $(".resume-container").offset().top - 50
+					}, 500);
+		},
 
-			});
-			require(['ui/resume/resumeView'], function(HomeView){
-
-				new HomeView({layoutTemplate: appLayoutTemplate})
-
-			});
-			require(['ui/contact/contactView'], function(ContactView){
-
-				new ContactView({layoutTemplate: appLayoutTemplate})
-
-			});
-		}
+		contact: function() {
+			$('html, body').animate({
+							scrollTop: $(".contact-container").offset().top - 50
+					}, 500);
+		},
 
 	});
 
